0feea76a441a4b148933923953e7d72b4d27b146
MrUnwonted <arjungodzarjun@gmail.com>
2024-01-31 13:52:34 +0530

    Adv Js

---
+++ advScript.js
@@ -0,0 +1,63 @@
+// Spread Operator
+
+// const globalPlayer ={
+//     id:0,
+//     name:"Bot",
+//     powerLevel:100,
+// }
+
+
+// const player1 = {...globalPlayer,powerLevel:2}
+// // player1.powerLevel=50
+// console.log(player1);
+
+// console.log(globalPlayer);
+
+// const globalPlayer1 = ['Arjun','Joel']
+// const globalPlayer2 = ['Althuaf']
+
+// const players =  [...globalPlayer1,'and', ...globalPlayer2]
+// console.log(players)
+
+// Rest Operator
+
+// const globalPlayer1 = ['Arjun','Joel']
+
+// const [first,second] = globalPlayer1
+
+// console.log(first);
+
+
+// const globalPlayer = {
+//     id:0,
+//     names:'Master Splinter',
+//     age:20
+// }
+
+// const{id,...end} = globalPlayer
+
+// console.log(id);
+// console.log(end);
+
+// Arrow
+// const addNumbers = (a,b)=> a+b;
+// console.log(addNumbers(3,4));
+
+// const multiplyNumbers = (x,y) => y*x;
+// console.log(multiplyNumbers(3,4))
+
+// const objFunc = ()=> ({name:"John",age:36});
+// console.log(objFunc());
+
+// const arrFunc = ()=>[1,2,3];
+// console.log(arrFunc())
+
+// const  person = person => person + 5;
+// console.log(person(5));
+
+
+// ForEach
+// const fruits = ["apple","banana","orange"];
+// fruits.forEach((item,index) => {
+//     console.log(`I like ${item}`);
+// })
---
+++ basicScript.js
@@ -0,0 +1,86 @@
+// console.log('hi');
+
+// function myFunc(){
+    
+    
+//     return 5
+// }
+
+// console.log(myFunc()); //5
+
+
+// myFunc = (c) =>{
+//     return c;
+// }
+// myFunc = c => c; //same like above
+// console.log(myFunc(10));
+
+// alert("Blocked")
+
+//Object
+// let user = {
+//     name: "John",
+//     age: 32,
+//     city: "New York"
+// }
+// console.log(user);
+
+//Array
+// let myArray = ["John",32,"India"]
+// console.log(myArray);
+
+// String
+// let a = "Arjun c"
+// let b = a.slice(0,7)
+// console.log(b);
+// console.log(typeof(a))
+
+// let c = "15"
+// let d = 15
+// console.log(c===d)
+// d.toString
+// console.log(typeof(d.toString()))
+
+// let e = parseInt(c)
+// console.log(e)
+// console.log(typeof(e))
+
+//Array
+let myArray = ['A','B','C']
+
+// myArray.push('D')
+// myArray.unshift('0')
+
+// console.log(myArray);
+
+// // myArray.pop()
+// myArray.shift()
+
+// console.log(myArray);
+
+// myArray.splice(2,1)
+// console.log(myArray);
+
+// myArray.splice(2,0,"D","E")
+// console.log(myArray);
+
+// let newArray = myArray.slice(1)
+// newArray.sort();
+// newArray.reverse();
+// console.log(newArray); 
+
+// Loop
+
+
+
+//Events
+
+// function onAlert(){
+//     alert("You clicked me!")
+//     console.log("Clicked");
+// }
+
+// let btn = document.getElementById('btn')
+// addEventListener('click',function(){
+//     this.alert("clicked")
+// })
